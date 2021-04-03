2911f2a92d71b43a069e2b379574cfce72859e23
nao501 <79750877+nao501@users.noreply.github.com>
2021-04-03 17:36:42 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -6,7 +6,6 @@ def kimetsu_search(word,csv_name):
     # 検索対象取得
     df=pd.read_csv(f"./{csv_name}")
     source=list(df["name"])
-
     # 検索
     if word in source:
         print(f"『{word}』はあります")
@@ -15,11 +14,11 @@ def kimetsu_search(word,csv_name):
         print(f"『{word}』はありません")
         eel.view_log_js(f"『{word}』はありません")
         eel.view_log_js(f"『{word}』を追加します")
-        ##add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        # if add_flg=="1":
+        # 追加
+        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+        #if add_flg=="1":
         source.append(word)
-    
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./{csv_name}",encoding="utf_8-sig")
+    df.to_csv("./{csv_name}", encoding="utf_8-sig")
     print(source)
